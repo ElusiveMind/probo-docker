866a7e1f63a7151c09430dad80351152f8b6e301
Michael R. Bagnall <mrbagnall@icloud.com>
2017-12-26 21:18:42 -0500

    [PB-390] - Implement conditional gitlab.

--- sh/startup.sh
+++ sh/startup.sh
@@ -55,19 +55,6 @@ chown probo:probo /opt/probo/data
 envsubst < /opt/probo/yml/assets-default.yml > /opt/probo/probo-asset-receiver/asset-receiver.yml
 envsubst < /opt/probo/yml/probo-defaults.yml > /opt/probo/probo/defaults.yaml
 envsubst < /opt/probo/yml/container-manager.yml > /opt/probo/probo/container-manager.yml
-
-if [[ ! -z "${USE_GITHUB}" ]] && [ $USE_GITHUB = "1" ]; then
-  envsubst < /opt/probo/yml/github-defaults.yml > /opt/probo/probo/github-handler.yml
-fi
-
-if [[ ! -z "${USE_BITBUCKET}" ]] && [ $USE_BITBUCKET = "1" ]; then
-  envsubst < /opt/probo/yml/bitbucket-defaults.yml > /opt/probo/probo-bitbucket/bitbucket-handler.yml
-fi
-
-if [[ ! -z "${USE_GITLAB}" ]] && [ $USE_GITLAB = "1" ]; then
-  envsubst < /opt/probo/yml/gitlab-defaults.yml > /opt/probo/probo-gitlab/gitlab-handler.yml
-fi
-
 envsubst < /opt/probo/yml/loom-defaults.yml > /opt/probo/probo-loom/loom.yml
 envsubst < /opt/probo/yml/proxy-defaults.yml > /opt/probo/probo-proxy/proxy.yml
 envsubst < /opt/probo/yml/reaper-defaults.yml > /opt/probo/probo-reaper/reaper.yml
@@ -81,7 +68,22 @@ rethinkdb --daemon --no-http-admin --runuser probo --rungroup probo
 # start all of our probo processes as the probo user with the exception of
 # the proxy in case the proxy is run on port 80.
 su - probo -c '/opt/probo/probo/bin/probo container-manager -c /opt/probo/probo/container-manager.yml > /dev/null &'
-su - probo -c '/opt/probo/probo-bitbucket/bin/probo-bitbucket-handler -c /opt/probo/probo-bitbucket/bitbucket-handler.yml > /dev/null &'
+
+if [[ ! -z "${USE_GITHUB}" ]] && [ $USE_GITHUB = "1" ]; then
+  envsubst < /opt/probo/yml/github-defaults.yml > /opt/probo/probo/github-handler.yml
+  su - probo -c '/opt/probo/probo/bin/probo github-handler -c /opt/probo/probo/github-handler.yml > /dev/null &'
+fi
+
+if [[ ! -z "${USE_BITBUCKET}" ]] && [ $USE_BITBUCKET = "1" ]; then
+  envsubst < /opt/probo/yml/bitbucket-defaults.yml > /opt/probo/probo-bitbucket/bitbucket-handler.yml
+  su - probo -c '/opt/probo/probo-bitbucket/bin/probo-bitbucket-handler -c /opt/probo/probo-bitbucket/bitbucket-handler.yml > /dev/null &'
+fi
+
+if [[ ! -z "${USE_GITLAB}" ]] && [ $USE_GITLAB = "1" ]; then
+  envsubst < /opt/probo/yml/gitlab-defaults.yml > /opt/probo/probo-gitlab/gitlab-handler.yml
+  su - probo -c '/opt/probo/probo-gitlab/bin/probo-gitlab-handler -c /opt/probo/probo-gitlab/gitlab-handler.yml > /dev/null &'
+fi
+
 su - probo -c '/opt/probo/probo-asset-receiver/bin/probo-asset-receiver -c /opt/probo/probo-asset-receiver/asset-receiver.yml > /dev/null &'
 su - probo -c 'mkdir /opt/probo/probo-loom/data'
 su - probo -c 'chmod 777 /opt/probo/probo-loom/data'
